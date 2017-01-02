bfac30916fede7c74b7410980ec5342103bf9948
Justin Collins <jbc13rds@gmail.com>
2017-01-02 14:40:52 -0600

    Created file to create the recipe db and insert a test record.

---
+++ dbcreatepopulate.sql
@@ -0,0 +1,27 @@
+/*
+CREATE SEQUENCE id_seq;
+
+CREATE TABLE recipe (
+    id INTEGER NOT NULL default nextval('id_seq'),
+    name VARCHAR(100) NOT NULL,
+    ingredients jsonb,
+    description VARCHAR(1000),
+    instructions VARCHAR(1000),
+    magazine VARCHAR(200)
+);
+
+ALTER SEQUENCE id_seq owned by recipe.id;
+*/
+
+/*
+insert into recipe (name,ingredients,description,instructions,magazine)
+values ('test'
+        ,'{"name":"sugar", "amount": "1tbsp"}'
+        ,'test recipe with test ingredients'
+        ,'mix the ingredients, then cook it'
+        ,'test magazine'
+        )
+        */
+
+select *
+from recipe
